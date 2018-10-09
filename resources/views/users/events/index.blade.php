827244fc8c3d81beada336e9f5edf94de16aa487
dimaskpz <dimaskpz@gmail.com>
2018-10-09 11:44:25 +0700

    events halaman. tab +

--- resources/views/users/events/index.blade.php
+++ resources/views/users/events/index.blade.php
@@ -13,212 +13,53 @@ Event
 
 @section('content')
 
-    <div class="w3-row-padding w3-margin-bottom">
-      <div class="w3-quarter">
-        <div class="w3-container w3-red w3-padding-16">
-          <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
-          <div class="w3-right">
-            <h3>52</h3>
-          </div>
-          <div class="w3-clear"></div>
-          <h4>Messages</h4>
-        </div>
-      </div>
-      <div class="w3-quarter">
-        <div class="w3-container w3-blue w3-padding-16">
-          <div class="w3-left"><i class="fa fa-eye w3-xxxlarge"></i></div>
-          <div class="w3-right">
-            <h3>99</h3>
-          </div>
-          <div class="w3-clear"></div>
-          <h4>Views</h4>
-        </div>
-      </div>
-      <div class="w3-quarter">
-        <div class="w3-container w3-teal w3-padding-16">
-          <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
-          <div class="w3-right">
-            <h3>23</h3>
-          </div>
-          <div class="w3-clear"></div>
-          <h4>Shares</h4>
-        </div>
-      </div>
-      <div class="w3-quarter">
-        <div class="w3-container w3-orange w3-text-white w3-padding-16">
-          <div class="w3-left"><i class="fa fa-users w3-xxxlarge"></i></div>
-          <div class="w3-right">
-            <h3>50</h3>
-          </div>
-          <div class="w3-clear"></div>
-          <h4>Users</h4>
-        </div>
-      </div>
+  <h2>Tabs in a Grid</h2>
+
+<input type="text" placeholder="Search.." name="search">
+
+    <div class="w3-row">
+      <a href="javascript:void(0)" onclick="openCity(event, 'tabaktif');">
+        <div class="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">EVENT AKTIF</div>
+      </a>
+      <a href="javascript:void(0)" onclick="openCity(event, 'tablalu');">
+        <div class="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">EVENT LALU</div>
+      </a>
+      {{-- <a href="javascript:void(0)" onclick="openCity(event, 'Tokyo');">
+        <div class="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">Tokyo</div>
+      </a> --}}
     </div>
 
-    <div class="w3-panel">
-      <div class="w3-row-padding" style="margin:0 -16px">
-        <div class="w3-third">
-          <h5>Regions</h5>
-          <img src="/w3images/region.jpg" style="width:100%" alt="Google Regional Map">
-        </div>
-        <div class="w3-twothird">
-          <h5>Feeds</h5>
-          <table class="w3-table w3-striped w3-white">
-            <tr>
-              <td><i class="fa fa-user w3-text-blue w3-large"></i></td>
-              <td>New record, over 90 views.</td>
-              <td><i>10 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-bell w3-text-red w3-large"></i></td>
-              <td>Database error.</td>
-              <td><i>15 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-users w3-text-yellow w3-large"></i></td>
-              <td>New record, over 40 users.</td>
-              <td><i>17 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-comment w3-text-red w3-large"></i></td>
-              <td>New comments.</td>
-              <td><i>25 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-bookmark w3-text-blue w3-large"></i></td>
-              <td>Check transactions.</td>
-              <td><i>28 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-laptop w3-text-red w3-large"></i></td>
-              <td>CPU overload.</td>
-              <td><i>35 mins</i></td>
-            </tr>
-            <tr>
-              <td><i class="fa fa-share-alt w3-text-green w3-large"></i></td>
-              <td>New shares.</td>
-              <td><i>39 mins</i></td>
-            </tr>
-          </table>
-        </div>
-      </div>
+    <div id="tabaktif" class="w3-container city" style="display:none">
+      <h2>London</h2>
+      <p>London is the capital city of England.</p>
     </div>
-    <hr>
-    <div class="w3-container">
-      <h5>General Stats</h5>
-      <p>New Visitors</p>
-      <div class="w3-grey">
-        <div class="w3-container w3-center w3-padding w3-green" style="width:25%">+25%</div>
-      </div>
 
-      <p>New Users</p>
-      <div class="w3-grey">
-        <div class="w3-container w3-center w3-padding w3-orange" style="width:50%">50%</div>
-      </div>
+    <div id="tablalu" class="w3-container city" style="display:none">
+      <h2>Paris</h2>
+      <p>Paris is the capital of France.</p>
+    </div>
 
-      <p>Bounce Rate</p>
-      <div class="w3-grey">
-        <div class="w3-container w3-center w3-padding w3-red" style="width:75%">75%</div>
-      </div>
-    </div>
-    <hr>
+    {{-- <div id="Tokyo" class="w3-container city" style="display:none">
+      <h2>Tokyo</h2>
+      <p>Tokyo is the capital of Japan.</p>
+    </div> --}}
+  </div>
 
-    <div class="w3-container">
-      <h5>Countries</h5>
-      <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
-        <tr>
-          <td>United States</td>
-          <td>65%</td>
-        </tr>
-        <tr>
-          <td>UK</td>
-          <td>15.7%</td>
-        </tr>
-        <tr>
-          <td>Russia</td>
-          <td>5.6%</td>
-        </tr>
-        <tr>
-          <td>Spain</td>
-          <td>2.1%</td>
-        </tr>
-        <tr>
-          <td>India</td>
-          <td>1.9%</td>
-        </tr>
-        <tr>
-          <td>France</td>
-          <td>1.5%</td>
-        </tr>
-      </table><br>
-      <button class="w3-button w3-dark-grey">More Countries  <i class="fa fa-arrow-right"></i></button>
-    </div>
-    <hr>
-    <div class="w3-container">
-      <h5>Recent Users</h5>
-      <ul class="w3-ul w3-card-4 w3-white">
-        <li class="w3-padding-16">
-          <img src="/w3images/avatar2.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-          <span class="w3-xlarge">Mike</span><br>
-        </li>
-        <li class="w3-padding-16">
-          <img src="/w3images/avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-          <span class="w3-xlarge">Jill</span><br>
-        </li>
-        <li class="w3-padding-16">
-          <img src="/w3images/avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-          <span class="w3-xlarge">Jane</span><br>
-        </li>
-      </ul>
-    </div>
-    <hr>
+  <script>
+  function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-red";
+  }
+  </script>
 
-    <div class="w3-container">
-      <h5>Recent Comments</h5>
-      <div class="w3-row">
-        <div class="w3-col m2 text-center">
-          <img class="w3-circle" src="/w3images/avatar3.png" style="width:96px;height:96px">
-        </div>
-        <div class="w3-col m10 w3-container">
-          <h4>John <span class="w3-opacity w3-medium">Sep 29, 2014, 9:12 PM</span></h4>
-          <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><br>
-        </div>
-      </div>
-
-      <div class="w3-row">
-        <div class="w3-col m2 text-center">
-          <img class="w3-circle" src="/w3images/avatar1.png" style="width:96px;height:96px">
-        </div>
-        <div class="w3-col m10 w3-container">
-          <h4>Bo <span class="w3-opacity w3-medium">Sep 28, 2014, 10:15 PM</span></h4>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><br>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="w3-container w3-dark-grey w3-padding-32">
-      <div class="w3-row">
-        <div class="w3-container w3-third">
-          <h5 class="w3-bottombar w3-border-green">Demographic</h5>
-          <p>Language</p>
-          <p>Country</p>
-          <p>City</p>
-        </div>
-        <div class="w3-container w3-third">
-          <h5 class="w3-bottombar w3-border-red">System</h5>
-          <p>Browser</p>
-          <p>OS</p>
-          <p>More</p>
-        </div>
-        <div class="w3-container w3-third">
-          <h5 class="w3-bottombar w3-border-orange">Target</h5>
-          <p>Users</p>
-          <p>Active</p>
-          <p>Geo</p>
-          <p>Interests</p>
-        </div>
-      </div>
-    </div>
 
 @endsection
