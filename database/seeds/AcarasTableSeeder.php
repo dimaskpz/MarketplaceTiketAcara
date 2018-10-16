8116b43460afb200d7258b6e9cb0c9b262f4a9f3
dimaskpz <dimaskpz@gmail.com>
2018-10-16 11:10:00 +0700

    seeder

---
+++ database/seeds/AcarasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AcarasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
